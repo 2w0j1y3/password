6cc69135b7935513417f7ce70398a31055a41780
2w0j1y3 <wczly1988@163.com>
2025-12-16 14:28:58 +0800

    Add files via upload

---
+++ 加密.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline string outb(int x){
+    int y=rand()%4+4;
+    string s;
+    while(x){
+        s=char(x%y+'0')+s;
+        x/=y;
+    }
+    s=char(y+'0')+s;
+    s=char('8'+rand()%2)+s;
+    return s;
+}
+int main() 
+{
+    freopen("text.txt","r",stdin);
+    freopen("work.txt","w",stdout);
+    srand(time(0));
+    int fir=rand();
+    cout<<outb(fir);
+    while(1){
+        char c=getchar();
+        if(c==EOF)break;
+        cout<<outb(c+fir);
+    }
+    return 0;
+}
---
+++ 解密.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+inline int getb(){
+    char c;c=getchar();
+    if(c==EOF)return -1;
+    int y=c-'0',x=0;
+    while(1){
+        c=getchar();
+        if(c>='8'||c==EOF)break;
+        x=x*y+c-'0';
+    }
+    return x;
+}
+int main() {
+    freopen("text.txt","r",stdin);
+    freopen("work.txt","w",stdout);
+    char c;c=getchar();
+    int fir=getb();
+    while(1){
+        int x=getb();
+        if(x==-1)break;
+        printf("%c",char(x-fir));
+    }
+}
